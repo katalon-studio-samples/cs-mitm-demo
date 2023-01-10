722c2aef396eb10c97b20bb9f40183a88cd923dc
Coty Rosenblath <coty@pobox.com>
2023-01-10 15:49:07 +0700

    Initial commit

---
+++ Scripts/Selenium SSL Proxy Test/Script1673246494780.groovy
@@ -0,0 +1,47 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+import org.openqa.selenium.edge.EdgeOptions as EdgeOptions
+import org.openqa.selenium.edge.EdgeDriver as EdgeDriver
+import org.openqa.selenium.remote.CapabilityType as CapabilityType
+import org.openqa.selenium.Proxy as Proxy
+import com.kms.katalon.core.webui.driver.DriverFactory as DriverFactory
+
+Proxy seleniumProxy = GlobalVariable.proxy
+
+EdgeOptions options = new EdgeOptions()
+
+options.setCapability(CapabilityType.ACCEPT_INSECURE_CERTS, true)
+
+options.setProxy(seleniumProxy)
+
+String edgeChromiumDriverPath = DriverFactory.getEdgeChromiumDriverPath()
+System.setProperty('webdriver.edge.driver', edgeChromiumDriverPath)
+
+// start the browser up
+driver = new EdgeDriver(options)
+
+DriverFactory.changeWebDriver(driver)
+
+WebUI.navigateToUrl('https://local.foobar3000.com:8443/server-app/data')
+
+WebUI.verifyTextPresent('Hello from Server-App-data method', false)
+
+println("Done")
+
---
+++ Test Suites/2-Way SSL Test Suite.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>2-Way SSL Test Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>e1416ad5-bd4f-4a3f-9a3f-793b337e4b68</testSuiteGuid>
+   <testCaseLink>
+      <guid>e3d88211-548c-4693-b2f3-0c8b83adc826</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Selenium SSL Proxy Test</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
